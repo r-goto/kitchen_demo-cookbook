8f36de691235d8576e07d3dbdc129ca31f7b9c62
r-goto <r-goto@myMac-16.local>
2022-04-06 16:41:00 +0900

    minor fixes/changes

--- compliance/profiles/windows_basic_tests/controls/cookbook_version_check.rb
+++ compliance/profiles/windows_basic_tests/controls/cookbook_version_check.rb
@@ -3,14 +3,13 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
 
-cookboko_version = '1.0.0'
-
-title '適用されたCookbookバージョンの確認'
-
 chef_node = input('chef_node', description: 'Chef Node')
+puts "\n##############################################\nConverged Cookbook バージョン: v#{chef_node['cookbooks']['kitchen_demo_cookbook']['version']}\n##############################################"
 
-if chef_node['cookbooks']['kitchen_demo_cookbook']['version'] != cookboko_version
-  raise Exception, "Cookbook バージョンが正常ではありません。Converged Version: #{chef_node['cookbooks']['kitchen_demo_cookbook']['version']}"
-else
-  p "Cookbook Version: v#{chef_node['cookbooks']['kitchen_demo_cookbook']['version']} Converged Successfully"
-end
+# title '適用されたCookbookバージョンの確認'
+# cookbook_version = '1.0.0'
+# if chef_node['cookbooks']['kitchen_demo_cookbook']['version'] != cookbook_version
+#   raise Exception, "Cookbook バージョンが正常ではありません。Converged Version: #{chef_node['cookbooks']['kitchen_demo_cookbook']['version']}"
+# else
+#   puts "\n##############################################\nConverged Cookbook バージョン: v#{chef_node['cookbooks']['kitchen_demo_cookbook']['version']}\n##############################################"
+# end
